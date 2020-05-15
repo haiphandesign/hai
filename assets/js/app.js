5a40d893290d8e7f464a809ca549a45909147111
Hải Phan <hai@haiphan.design>
2020-05-16 08:03:29 +1000

    It's 8AM

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,37 @@ $('.toggle').click(function (e) {
         $(this).addClass('is-active');
     }
 });
+
+
+
+
+// target elements with the "draggable" class
+interact('.draggable')
+    .draggable({
+        listeners: {
+            move: dragMoveListener
+        }
+    })
+
+
+
+function dragMoveListener(event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+    var r = (parseFloat(target.getAttribute('data-r')) || 0)
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px) rotate(' + r + 'deg)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+
+window.dragMoveListener = dragMoveListener
